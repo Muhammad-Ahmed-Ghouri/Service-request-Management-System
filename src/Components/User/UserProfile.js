0b796d3d506b056ee03d8ecac32b591d88226014
Muhammad-Ahmed-Ghouri <muhammadahmed7274@gmail.com>
2026-01-01 21:39:18 +0500

    Edit UI of user's profile

--- src/Components/User/UserProfile.js
+++ src/Components/User/UserProfile.js
@@ -1,4 +1,12 @@
-import { Avatar, Box, Divider, Grid, Paper, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Grid,
+  Container,
+  Paper,
+  Typography,
+} from "@mui/material";
 
 export default function UserProfile() {
   // Later this data will come from API
@@ -8,12 +16,17 @@ export default function UserProfile() {
     email: "ahmed@example.com",
     phone: "+92 300 1234567",
     role: "User",
-    joinedOn: "01 Jan 2026",
+    password: "@ahmed123",
   };
 
   return (
     <Box>
-      <Typography variant="h5" fontWeight={600} mb={2}>
+      <Typography
+        variant="h4"
+        fontWeight={500}
+        sx={{ color: "#06395dff" }}
+        mb={2}
+      >
         My Profile
       </Typography>
 
@@ -33,7 +46,11 @@ export default function UserProfile() {
           </Avatar>
 
           <Box>
-            <Typography variant="h6" fontWeight={600}>
+            <Typography
+              variant="h6"
+              fontWeight={600}
+              sx={{ color: "#06395dff" }}
+            >
               {user.name}
             </Typography>
             <Typography color="text.secondary">@{user.username}</Typography>
@@ -43,14 +60,48 @@ export default function UserProfile() {
         <Divider sx={{ mb: 3 }} />
 
         {/* Details Section */}
-        <Grid container spacing={2}>
-          <ProfileItem label="Full Name" value={user.name} />
-          <ProfileItem label="Username" value={user.username} />
-          <ProfileItem label="Email" value={user.email} />
-          <ProfileItem label="Phone" value={user.phone} />
-          <ProfileItem label="Role" value={user.role} />
-          <ProfileItem label="Joined On" value={user.joinedOn} />
-        </Grid>
+        <Container sx={{ display: "flex", padding: "0" }}>
+          <Box
+            container
+            spacing={2}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              border: "3px dotted rgba(222, 223, 223, 1)",
+              borderRadius: "10px",
+              backgroundColor: "#cbf5ff",
+              padding: "15px",
+              margin: "0 15px 0 0",
+              width: "50%",
+              justifyContent: "space-around",
+              height: "150px",
+            }}
+          >
+            <ProfileItem label="Full Name" value={user.name} />
+            <ProfileItem label="Email" value={user.email} />
+            <ProfileItem label="Phone" value={user.phone} />
+          </Box>
+          <Box
+            container
+            spacing={2}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              border: "3px dotted rgba(222, 223, 223, 1)",
+              borderRadius: "10px",
+              backgroundColor: "#cbf5ff",
+              padding: "15px",
+              width: "50%",
+              justifyContent: "space-around",
+              height: "150px",
+              margin: "0 0 0 15px",
+            }}
+          >
+            <ProfileItem label="Username" value={user.username} />
+            <ProfileItem label="Password" value={user.password} />
+            <ProfileItem label="Role" value={user.role} />
+          </Box>
+        </Container>
       </Paper>
     </Box>
   );
